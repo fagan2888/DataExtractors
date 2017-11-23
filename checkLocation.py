322e3761157229031d4ff665e710010d23b000d7
Tony Sanchez <29736946+aosanchez@users.noreply.github.com>
2017-11-23 09:44:20 -0500

    Updated checkLocation.py file with comments

--- checkLocation.py
+++ checkLocation.py
@@ -1,12 +1,20 @@
 """
-Code written to convert Lat/Lon to neighborhood_cluster
+Tony Sanchez
+10/20/2017
+Georgetown Cohort 10: The Data Extractors Team
+
+Description:
+This Python code was written to convert extracted Lat/Lon values
+to a designated neighborhood_cluster, used in other parts of the project.
+neighborhood_cluster is the shapefile grouping we are using to compare and contrast
+each neighborhood to identify growth and decline.
 """
 
 import shapefile
-import geoid
 from shapely.geometry import Point, shape
-
-
+"""
+Returns neighborhood_cluster value as well as the description of the neighborhood.
+"""
 def getNeighborhoodClusterLatLon(path, lat, lon):
     # read your shapefile
     sf = shapefile.Reader(path)
@@ -29,7 +37,9 @@ def getNeighborhoodClusterLatLon(path, lat, lon):
 
 
 if __name__ == '__main__':
-    # Test code that loads shapefile and returns a neighborhood_cluster
+    """
+    Test code that loads a local shapefile and returns a neighborhood_cluster location
+    """
     lat = 0
     lon = 0
     print("latitude is: " + str(lat))
